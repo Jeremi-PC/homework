8fef0c2628479fcec0cbf66f68c92245ff09b3e1
Jeremi-PC <127249581+Jeremi-PC@users.noreply.github.com>
2023-04-07 16:02:07 +0200

    Add file homework #1

---
+++ HomeWork1.java
@@ -0,0 +1,12 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        int a = 585;
+        int h = a / 100;
+        int d = (a % 100) / 10;
+        int u = (a % 100) % 10;
+        System.out.println(h);
+        System.out.println(d);
+        System.out.println(u);
+
+    }
+}
